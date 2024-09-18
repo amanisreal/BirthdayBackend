c77fd1d03b778c1ee006e0528b411424593e140a
Aman <amanborkar995@gmail.com>
2024-09-18 18:20:40 +0530

    cors

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -17,6 +17,11 @@ router.post('/createUser', async (req, res) => {
 })
 
 router.get('/getUser', auth, async(req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader("Access-Control-Allow-Headers", "content-type");
+    res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
     try{
         res.send(req.user)
     }catch(error){
