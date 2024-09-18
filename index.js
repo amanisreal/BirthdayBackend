c08d414574816a3e82917ae9a912712cfc7686de
Aman <amanborkar995@gmail.com>
2024-09-18 16:56:40 +0530

    cors

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRouter = require('./routes/userRoute');
 const connectBD = require('./database/mongoose')
 require('dotenv').config()
+const cors = require('cors');
 
 const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 const connection = async () => {
     try{
