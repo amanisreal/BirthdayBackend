f2d9c7c9f4a95a46f45cfa11c81cb2ab0e104f65
Aman <amanborkar995@gmail.com>
2024-09-18 18:44:43 +0530

    cookie

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const userRouter = require('./routes/userRoute');
 const connectBD = require('./database/mongoose')
 require('dotenv').config()
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
 const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(userRouter);
 const connection = async () => {
     try{
